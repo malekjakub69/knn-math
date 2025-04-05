0692e891690250dd249238e33447ebed3eb1de81
marekvecera <veceramarek02@gmail.com>
2025-04-05 20:06:14 +0200

    inkml2img working

--- app/neural_network/inkml2img.py
+++ app/neural_network/inkml2img.py
@@ -1,12 +1,17 @@
 import numpy as np
-from skimage.draw import line
-from skimage.morphology import thin
 import matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
+from PIL import Image, ImageDraw
 
 
 def get_traces_data(inkml_file_abs_path, xmlns='{http://www.w3.org/2003/InkML}'):
-
+    """
+    Extracs and returns X Y coordinates from InkML file + ground truth formula in form of string.
+    :param inkml_file_abs_path: path to InkML file
+    :param xmlns: XML namespace
+    :return: traces_data - list of dictionaries with coordinates and labels
+    :return: formula - ground truth formula
+    """
     traces_data = []
 
     tree = ET.parse(inkml_file_abs_path)
@@ -16,17 +21,10 @@ def get_traces_data(inkml_file_abs_path, xmlns='{http://www.w3.org/2003/InkML}')
 
     'Stores traces_all with their corresponding id'
     'Remove 3rd coordinate - speed in m/s'
-    # traces_all = [{'id': trace_tag.get('id'),
-    #                'coords': [[round(float(axis_coord)) if float(axis_coord).is_integer() else round(float(axis_coord) * 10000)
-    #                            for axis_coord in coord[1:].split(' ')] if coord.startswith(' ')
-    #                           else [round(float(axis_coord)) if float(axis_coord).is_integer() else round(float(axis_coord) * 10000)
-    #                                 for axis_coord in coord.split(' ')]
-    #                           for coord in (trace_tag.text).replace('\n', '').split(',')]}
-    #               for trace_tag in root.findall(doc_namespace + 'trace')]
     traces_all = [{'id': trace_tag.get('id'),
-                    'coords': [[round(float(axis_coord) * 10000)
+                    'coords': [[round(float(axis_coord))
                                 for axis_coord in coord[1:].split(' ')[:2]] if coord.startswith(' ')
-                                else [round(float(axis_coord) * 10000)
+                                else [round(float(axis_coord))
                                     for axis_coord in coord.split(' ')[:2]]
                                 for coord in (trace_tag.text).replace('\n', '').split(',')]}
                     for trace_tag in root.findall(doc_namespace + 'trace')]
@@ -57,86 +55,82 @@ def get_traces_data(inkml_file_abs_path, xmlns='{http://www.w3.org/2003/InkML}')
 
     else:
         'Consider Validation data that has no labels'
+        'Our case - traces aren\'t grouped into traceGroups'
+        # for idx, trace in enumerate(traces_all):
+        #     traces_data.append({'trace_group': [trace['coords']]})
         [traces_data.append({'trace_group': [trace['coords']]})
          for trace in traces_all]
         # for idx, trace in enumerate(traces_all):
         #     traces_data.append({'label': str(idx), 'trace_group': [trace['coords']]})
 
-    return traces_data
+    'Extract the ground truth from normalizedLabel'
+    formula = None
+    annotations = root.findall(doc_namespace + 'annotation')
+    if annotations:
+        for annotation in annotations:
+            if annotation.get('type') == 'normalizedLabel':
+                formula = annotation.text
+
+    return traces_data, formula
 
 
-def convert_to_imgs(traces_data, box_size=int(100)):
+def inkml2img(inkml_file_abs_path, img_height=int(80), line_width=int(1), padding=int(0), color="black"):
+    """
+    Converts InkML file to an image with specified height and line width.
+    :param inkml_file_abs_path: path to InkML file
+    :param img_height: height of the output image (width is variable)
+    :param line_width: width of the lines in the image
+    :param padding: padding around the image
+    :return: img - PIL Image object
+    :return: latex - ground truth formula
+    """
+    traces_data, formula = get_traces_data(inkml_file_abs_path)
+    all_traces = []
+    [all_traces.append(trace_grp['trace_group']) for trace_grp in traces_data]
+    all_traces_collapsed = [trace for trace_group in all_traces for trace in trace_group]
 
-    patterns_enc = []
-    classes_rejected = []
+    # Compute min and max coordinates for all traces
+    min_x, min_y, max_x, max_y = get_min_coords(all_traces_collapsed)
 
-    for pattern in traces_data:
+    # Shift the trace group to align with (0, 0)
+    all_traces = [shift_trace_grp(trace, min_x=min_x, min_y=min_y) for trace in all_traces]
 
-        trace_group = pattern['trace_group']
+    # Interpolate the traces to have a height of 80 pixels
+    trace_height = max_y - min_y
+    trace_width = max_x - min_x
+    all_traces = [interpolate(trace_group, trace_grp_height=trace_height, trace_grp_width=trace_width, box_size=img_height - 1 - (padding*2)) for trace_group in all_traces]
 
-        'mid coords needed to shift the pattern'
-        min_x, min_y, max_x, max_y = get_min_coords(trace_group)
+    # Compute the new width after interpolation
+    new_width = max([max([coord[0] for coord in trace]) for trace_group in all_traces for trace in trace_group]) + 1 + (padding*2)
+    new_height = img_height  # Fixed height
 
-        'traceGroup dimensions'
-        trace_grp_height, trace_grp_width = max_y - min_y, max_x - min_x
+    # Create a blank white image
+    img = Image.new("RGB", (new_width, new_height), "white")
+    draw = ImageDraw.Draw(img)
 
-        'shift pattern to its relative position'
-        shifted_trace_grp = shift_trace_grp(
-            trace_group, min_x=min_x, min_y=min_y)
-
-        'Interpolates a pattern so that it fits into a box with specified size'
-        'method: LINEAR INTERPOLATION'
-        try:
-            interpolated_trace_grp = interpolate(shifted_trace_grp,
-                                                 trace_grp_height=trace_grp_height, trace_grp_width=trace_grp_width, box_size=box_size - 1)
-        except Exception as e:
-            print(e)
-            print('This data is corrupted - skipping.')
-            classes_rejected.append(pattern.get('label'))
-
-            continue
-
-        'Get min, max coords once again in order to center scaled patter inside the box'
-        min_x, min_y, max_x, max_y = get_min_coords(interpolated_trace_grp)
-
-        centered_trace_grp = center_pattern(
-            interpolated_trace_grp, max_x=max_x, max_y=max_y, box_size=box_size)
-
-        'Center scaled pattern so it fits a box with specified size'
-        pattern_drawn = draw_pattern(centered_trace_grp, box_size=box_size)
-        # Make sure that patterns are thinned (1 pixel thick)
-        pat_thinned = 1.0 - thin(1.0 - np.asarray(pattern_drawn))
-        plt.imshow(pat_thinned, cmap='gray')
-        plt.show()
-        pattern_enc = dict(
-            {'features': pat_thinned, 'label': pattern.get('label')})
-
-        # Filter classes that belong to categories selected by the user
-#             if pattern_enc.get('label') in self.classes:
-
-        patterns_enc.append(pattern_enc)
-
-    return patterns_enc, classes_rejected
+    # Draw the traces on the image
+    for trace_group in all_traces:
+        for trace in trace_group:
+            if len(trace) == 1:
+                # Draw a single point
+                x, y = trace[0]
+                draw.point((x+padding, y+padding), fill=color, size=line_width)
+            else:
+                # Draw lines connecting the points
+                for i in range(len(trace) - 1):
+                    x1, y1 = trace[i]
+                    x2, y2 = trace[i + 1]
+                    draw.line((x1+padding, y1+padding, x2+padding, y2+padding), fill=color, width=line_width)
+    return img, formula
 
 
 def get_min_coords(trace_group):
+    min_x_coords = min([coord[0] for trace in trace_group for coord in trace])
+    min_y_coords = min([coord[1] for trace in trace_group for coord in trace])
+    max_x_coords = max([coord[0] for trace in trace_group for coord in trace])
+    max_y_coords = max([coord[1] for trace in trace_group for coord in trace])
 
-    min_x_coords = []
-    min_y_coords = []
-    max_x_coords = []
-    max_y_coords = []
-
-    for trace in trace_group:
-
-        x_coords = [coord[0] for coord in trace]
-        y_coords = [coord[1] for coord in trace]
-
-        min_x_coords.append(min(x_coords))
-        min_y_coords.append(min(y_coords))
-        max_x_coords.append(max(x_coords))
-        max_y_coords.append(max(y_coords))
-
-    return min(min_x_coords), min(min_y_coords), max(max_x_coords), max(max_y_coords)
+    return min_x_coords, min_y_coords, max_x_coords, max_y_coords
 
 
 def shift_trace_grp(trace_group, min_x, min_y):
@@ -161,17 +155,8 @@ def interpolate(trace_group, trace_grp_height, trace_grp_width, box_size):
     if trace_grp_width == 0:
         trace_grp_width += 1
 
-    '' 'KEEP original size ratio' ''
-    trace_grp_ratio = (trace_grp_width) / (trace_grp_height)
-
-    scale_factor = 1.0
-    '' 'Set \"rescale coefficient\" magnitude' ''
-    if trace_grp_ratio < 1.0:
-
-        scale_factor = (box_size / trace_grp_height)
-    else:
-
-        scale_factor = (box_size / trace_grp_width)
+    'Scale for new heigh = box_size'
+    scale_factor = box_size / trace_grp_height
 
     for trace in trace_group:
         'coordintes convertion to int type necessary'
@@ -183,85 +168,9 @@ def interpolate(trace_group, trace_grp_height, trace_grp_width, box_size):
     return interpolated_trace_grp
 
 
-def get_min_coords(trace_group):
-
-    min_x_coords = []
-    min_y_coords = []
-    max_x_coords = []
-    max_y_coords = []
-
-    for trace in trace_group:
-
-        x_coords = [coord[0] for coord in trace]
-        y_coords = [coord[1] for coord in trace]
-
-        min_x_coords.append(min(x_coords))
-        min_y_coords.append(min(y_coords))
-        max_x_coords.append(max(x_coords))
-        max_y_coords.append(max(y_coords))
-
-    return min(min_x_coords), min(min_y_coords), max(max_x_coords), max(max_y_coords)
-
-
-def center_pattern(trace_group, max_x, max_y, box_size):
-
-    x_margin = int((box_size - max_x) / 2)
-    y_margin = int((box_size - max_y) / 2)
-
-    return shift_trace_grp(trace_group, min_x=-x_margin, min_y=-y_margin)
-
-
-def draw_pattern(trace_group, box_size):
-
-    pattern_drawn = np.ones(shape=(box_size, box_size), dtype=np.float32)
-    for trace in trace_group:
-
-        ' SINGLE POINT TO DRAW '
-        if len(trace) == 1:
-            x_coord = trace[0][0]
-            y_coord = trace[0][1]
-            pattern_drawn[y_coord, x_coord] = 0.0
-
-        else:
-            ' TRACE HAS MORE THAN 1 POINT '
-
-            'Iterate through list of traces endpoints'
-            for pt_idx in range(len(trace) - 1):
-                print(pt_idx, trace[pt_idx])
-
-                'Indices of pixels that belong to the line. May be used to directly index into an array'
-                pattern_drawn[line(r0=int(trace[pt_idx][1]), c0=int(trace[pt_idx][0]),
-                                   r1=int(trace[pt_idx + 1][1]), c1=int(trace[pt_idx + 1][0]))] = 0
-
-    return pattern_drawn
-
-
-def inkml2img(input_path, output_path, color='black'):
-    traces = get_traces_data(input_path)
-    # print(traces)
-    # print(get_min_coords(traces[0]['trace_group']))
-    for elem in traces:
-        ls = elem['trace_group']
-        for subls in ls:
-            data = np.array(subls)
-            x, y = zip(*data)
-            plt.plot(x, y, linewidth=2, c=color)
-            # plt.scatter(x, y, s=5, c=color)
- 
-    plt.gca().invert_yaxis()
-    plt.gca().set_aspect('equal', adjustable='box')
-    plt.gca().get_xaxis().set_visible(False)
-    plt.gca().get_yaxis().set_visible(False)
-    plt.gca().spines['top'].set_visible(False)
-    plt.gca().spines['right'].set_visible(False)
-    plt.gca().spines['bottom'].set_visible(False)
-    plt.gca().spines['left'].set_visible(False)
-    plt.savefig(output_path, bbox_inches='tight', dpi=100)
-    plt.gcf().clear()
-
-
 if __name__ == "__main__":
     import sys
     input_inkml = sys.argv[1]
-    output_path = sys.argv[2]
-    inkml2img(input_inkml, output_path)     # , color='#284054'
+    img = inkml2img(input_inkml, img_height=80, line_width=1, padding=2, color='#284054')
+    img.show()
+    print(img.size)
